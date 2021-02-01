6345fd79aa0da7aeee20d9e1fa40da91a14ee32a
Vilja Lott <vilja.lott@stud.tu-darmstadt.de>
2021-02-01 20:08:23 +0100

    extended fake data set

--- test-app/src/app/containers/dataProvider.js
+++ test-app/src/app/containers/dataProvider.js
@@ -30,30 +30,174 @@ const dataProvider = fakeDataProvider({
       size_: 'Durchmesser: 8,3 cm',
       location: 'Fundort: Landkreis Alzey-Worms',
     },
+    {
+      _id: 'Kg 42:67',
+      category: 'Kunst und Kulturgeschichte',
+      sub_category: 'Gotik',
+      title: 'Ortenberger Altar - Heilige Sippe',
+      picture: [
+        {
+          $oid: '5ff95692dcf3447f57146c1s',
+        },
+      ],
+      description:
+        'Tafelmalerei auf Tannenholz, mit Blattgold',
+      additional_information:
+        '',
+      interdisciplinary_context: 'Themen: Kunsthandwerk',
+      year: 'um 1430',
+      art_type: 'Gemälde',
+      creator: 'Meister des Ortenberger Altars',
+      material:
+        'Tannenholz, Leinwand, Tempera, Blattgold',
+      size_: '100,5 cm x 162,5 cm',
+      location: 'Ortenberg',
+    },
+    {
+      _id: 'Kg 2:95',
+      category: 'Kunst und Kulturgeschichte',
+      sub_category: 'Expressionsimus',
+      title: 'Morgensonne',
+      picture: [
+        {
+          $oid: '5ffe569ssd2dcf3447f5714561s',
+        },
+      ],
+      description:
+        'Gemälde',
+      additional_information:
+        'Öl auf Leinwand',
+      interdisciplinary_context: 'Themen: Gemälde',
+      year: '1910',
+      art_type: 'Gemälde',
+      creator: 'Lovis Corinth',
+      material:
+        'Öl, Leinwand',
+      size_: '68,5 cm x 80,5 cm',
+      location: 'Ortenberg',
+    },
+    {
+      _id: 'Kg 34:12',
+      category: 'Kunst und Kulturgeschichte',
+      sub_category: 'Realismus',
+      title: 'Abendstimmung in der Campagna',
+      picture: [
+        {
+          $oid: '5ffe569ssfesrsd447f5714561s',
+        },
+      ],
+      description:
+        'Gemälde',
+      additional_information:
+        'Öl auf Leinwand',
+      interdisciplinary_context: 'Themen: Gemälde',
+      year: '1850',
+      art_type: 'Gemälde',
+      creator: 'Oswald Achenbach',
+      material:
+        'Öl, Leinwand',
+      size_: '98 cm x 134 cm',
+      location: 'Düsseldorf',
+    },
+    {
+      _id: 'Kg 8:27',
+      category: 'Kunst und Kulturgeschichte',
+      sub_category: 'Symbolismus',
+      title: 'Gestade der Vergessenheit',
+      picture: [
+        {
+          $oid: '8sd95564dcf3675f57146gaw',
+        },
+      ],
+      description:
+        'Gestade der Vergessenheit (meist mit Artikel „Das…“, seltener „Die…“) ist ein Gemälde von Eugen Bracht. Zwischen 1889 und 1916 entstanden acht Fassungen. Von diesen sind mindestens zwei museal erhalten und werden permanent ausgestellt. Neben Arnold Böcklins Toteninsel gilt es als eines der bekanntesten Werke des Symbolismus.',
+      additional_information:
+        'Urversion',
+      interdisciplinary_context: 'Themen: Gemälde',
+      year: '1889',
+      art_type: 'Gemälde',
+      creator: 'Eugen Bracht',
+      material:
+        'Öl, Leinwand',
+      size_: '139 cm x 257 cm',
+      location: 'Hessen',
+    },
   ],
   Touren: [
     {
       _id: {
         $oid: '600024333c43791d7831f306',
       },
-      name: 'Tour1',
+      name: 'Amphibien Tour',
       search_id: 'xxxxx',
-      description: 'This is such an awesome tour',
+      description: 'Die Tour führt entlang der interessanter Exponate rund um Amphibien.',
       session_id: 420,
       owner: {
-        username: 'Anna',
+        username: 'Marie',
       },
       user: [
         {
-          username: 'Vilja',
+          username: 'Tom',
         },
         {
-          username: 'Cosima',
+          username: 'Lena',
         },
       ], 
       difficulty: 5,
       status: 'privat',
-      lastEdit: '2020-01-01',
+      lastEdit: '2021-01-01',
+    },
+    {
+      _id: {
+        $oid: '600024333c67891h7831f659',
+      },
+      name: 'Zoologie Tour - Australien',
+      search_id: 'xxxxx',
+      description: 'Die Tiere aus Down Under lassen sich in dieser Tour bestaunen.',
+      session_id: 435,
+      owner: {
+        username: 'Lena',
+      },
+      user: [
+        {
+          username: 'Lukas',
+        },
+        {
+          username: 'Franziska',
+        },
+        {
+          username: 'Tom',
+        },
+      ], 
+      difficulty: 2,
+      status: 'freigegeben',
+      lastEdit: '2020-10-11',
+    },
+    {
+      _id: {
+        $oid: '660024555c67891l6731f789',
+      },
+      name: 'Urpferd 2.0',
+      search_id: 'xxxxx',
+      description: 'Diese Tour dreht sich um das »Wappentier« der Grube Messel, das Messeler Urpferd ',
+      session_id: 375,
+      owner: {
+        username: 'Frank',
+      },
+      user: [
+        {
+          username: 'Lukas',
+        },
+        {
+          username: 'Franziska',
+        },
+        {
+          username: 'Tobias',
+        },
+      ], 
+      difficulty: 4,
+      status: 'ausstehend',
+      lastEdit: '2020-08-18',
     },
   ],
   Benutzer: [
@@ -84,6 +228,54 @@ const dataProvider = fakeDataProvider({
     {
       username: 'Lukas',
       password: 'L99geschi',
+      email: '',
+      Adminrechte: false,
+    },
+    {
+      username: 'Frank',
+      password: 'passwort',
+      email: '',
+      Adminrechte: true,
+    },
+    {
+      username: 'Sabine',
+      password: 'MyPassword',
+      email: '',
+      Adminrechte: false,
+    },
+    {
+      username: 'Simone',
+      password: 'hallo',
+      email: '',
+      Adminrechte: false,
+    },
+    {
+      username: 'Carlo',
+      password: 'meinPasswort',
+      email: '',
+      Adminrechte: false,
+    },
+    {
+      username: 'Tobias',
+      password: '4567',
+      email: '',
+      Adminrechte: false,
+    },
+    {
+      username: 'Markus',
+      password: 'hihi',
+      email: '',
+      Adminrechte: false,
+    },
+    {
+      username: 'Andreas',
+      password: 'hallsao',
+      email: '',
+      Adminrechte: false,
+    },
+    {
+      username: 'Lilli',
+      password: 'geschichte',
       email: '',
       Adminrechte: false,
     },
