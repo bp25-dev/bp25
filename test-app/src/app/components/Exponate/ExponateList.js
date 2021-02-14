4f281e22fdac9015950d89c35cf834c9269e5709
Anna Kugler <ar-annamaria.kugler@rakuten.com>
2021-02-14 10:01:36 +0100

    disconntected filter sidebar from exponate list class for better overview

--- test-app/src/app/components/Exponate/ExponateList.js
+++ test-app/src/app/components/Exponate/ExponateList.js
@@ -4,108 +4,22 @@ import {
   SingleFieldList,
   Datagrid,
   EditButton,
-  useListContext,
-  //Filter
-  FilterList,
-  FilterListItem,
-  FilterLiveSearch,
   //Shows
   SimpleShowLayout,
   Show,
   //Fields
   TextField,
   ImageField,
-  BooleanField,
-  ReferenceField,
   ReferenceArrayField,
 } from 'react-admin';
-// material UI styling
-import { Card as MuiCard, CardContent, withStyles } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-//icons
-import ImageSearchIcon from '@material-ui/icons/ImageSearch';
-import ClassIcon from '@material-ui/icons/Class';
-import PersonIcon from '@material-ui/icons/Person';
-import SchoolIcon from '@material-ui/icons/School';
-
-// filter sidebar styling
-const Card = withStyles((theme) => ({
-  root: {
-    [theme.breakpoints.up('sm')]: {
-      order: -1, // display on the left rather than on the right of the list
-      width: '15em',
-      minWidth: 250,
-      marginRight: '1em',
-    },
-    [theme.breakpoints.down('sm')]: {
-      display: 'none',
-    },
-  },
-}))(MuiCard);
-
-//TODO: more filters.. Subkategorie Mapping, Timeframes, Material, ...?
-// check if picture object is not empty (create boolean)
-const HasImageFilter = () => (
-  <FilterList label='Bildverknüpfung' icon={<ImageSearchIcon />}>
-    <FilterListItem label='vorhanden' value={{ img: true }} />
-    <FilterListItem label='nicht vorhanden' value={{ img: null }} />
-  </FilterList>
-);
-
-// .filter((a,b) => ids.indexOf(a) === b))
-
-// sidebar functionality
-const FilterSidebar = () => {
-  const { ids, data } = useListContext();
-  return (
-    <Card>
-      <CardContent>
-        <FilterLiveSearch />
-        <Typography variant="subtitle1" color='error' gutterBottom>
-        Entwurf: Bitte klicke auf keine von den unteren Kategorien 
-      </Typography>
-        <HasImageFilter />
-        <FilterList label='Subkategorie' icon={<ClassIcon />}>
-          {ids.map((id) => (
-            <FilterListItem
-              label={data[id].sub_category}
-              key={data[id].id}
-              value={{ groups: data[id].id }}
-            />
-          ))}
-        </FilterList>
-        <FilterList label='Ersteller' icon={<PersonIcon />}>
-          {ids.map((id) => (
-            <FilterListItem
-              label={data[id].creator}
-              key={data[id]._id}
-              value={{ groups: data[id]._id }}
-            />
-          ))}
-        </FilterList>
-        <FilterList label='Interdisziplinäre Bezüge' icon={<SchoolIcon />}>
-         {/*  {
-            ids.map((id) => (
-              <FilterListItem
-                label={data[id].interdisciplinary_context.split('Themen:')}
-                value={data[id]._id}
-              />
-            ))
-            .filter((a,b) => ids.indexOf(a) === b)
-          } */}
-        </FilterList>
-      </CardContent>
-    </Card>
-  );
-};
+import {FilterSidebar} from './ExponateFilterSidebar.js'
 
 // edit expand component
 const ImageShow = (props) => {
   return (
-    <Show
+    <Show 
       {...props}
-      /* disable the app title change when shown */
-      title=' '
+      title=' ' /* disable the app title change when shown */
     >
       <SimpleShowLayout>
         <TextField source='art_type' label='Kunsttyp' />
--- test-app/src/app/containers/MyProfile/MyLayout.js
+++ test-app/src/app/containers/MyProfile/MyLayout.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 import TreeMenu from '@bb-tech/ra-treemenu';
-import { Layout } from "react-admin";
-import MyAppBar from "./MyAppBar";
-import { ProfileProvider } from "./profile";
+import { Layout } from 'react-admin';
+import MyAppBar from './MyAppBar';
+import { ProfileProvider } from './profile';
 
 const MyLayout = (props) => (
   <ProfileProvider>
-    <Layout {...props} appBar={MyAppBar} menu={TreeMenu}/>
+    <Layout {...props} appBar={MyAppBar} menu={TreeMenu} />
   </ProfileProvider>
 );
 
