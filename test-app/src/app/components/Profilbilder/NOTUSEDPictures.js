2e6a5341402a96d3bf69c05fe38ed116bc8111ed
mosouka <cosima.balzer@stud.tu-darmstadt.de>
2021-03-04 19:07:49 +0100

    Revert to 69e533ee

---
+++ test-app/src/app/components/Profilbilder/NOTUSEDPictures.js
@@ -0,0 +1,66 @@
+/*import React from 'react';
+import {
+  List,
+  Datagrid,
+  UrlField,
+  ImageField,
+  ImageInput,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SimpleShowLayout,
+  Show,
+} from 'react-admin';
+import PictureGrid from './PictureGrid';
+
+
+// edit expand component
+const ImageShow = (props) => (
+  <Show
+    {...props}
+    /* disable the app title change when shown 
+    title=' '
+  >
+    <SimpleShowLayout>
+      <ImageField source='picture' />
+      <UrlField source='picture' label='Url' />
+    </SimpleShowLayout>
+  </Show>
+);
+
+// list existing badges
+export const PictureList = (props) => (
+  <List {...props} title='Picture'>
+    {/* <Datagrid expand={<ImageShow />}>
+      <TextField source='description' label='Beschreibung' />
+      <EditButton />
+    </Datagrid> */}
+    <PictureGrid/>
+  </List>
+);
+
+// edit badges
+export const PictureEdit = (props) => (
+  <Edit title='Verändere Profilbilder' {...props}>
+    <SimpleForm>
+      <ImageInput source='Picture' accept='image/*' placeholder={<p>Füge hier neue Profilbilder hinzu</p>}>
+        <ImageField source='picture'/>
+      </ImageInput>
+      <TextInput label='Beschreibung' source='description' />
+    </SimpleForm>
+  </Edit>
+);
+
+// create a new badge
+export const PictureCreate = (props) => (
+  <Create title='Lade Profilbilder hoch' {...props}>
+    <SimpleForm>
+      <ImageInput source='Picture' accept='image/*' placeholder={<p>Füge hier neue Profilbilder hinzu</p>}>
+        <ImageField source='picture' />
+      </ImageInput>
+      <TextInput label='Beschreibung' source='description' />
+    </SimpleForm>
+  </Create>
+);
+*/
