2863275de99d30f319e07fac89dae71d71a0f220
Anna Kugler <ar-annamaria.kugler@rakuten.com>
2021-03-04 16:17:45 +0100

    tried sample queries to API

--- test-app/src/app/data/dataProvider.js
+++ test-app/src/app/data/dataProvider.js
@@ -1,3 +1,49 @@
-import jsonServerProvider from 'ra-data-json-server';
+import jsonapiClient from 'ra-jsonapi-client';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com', httpClient);
+const apiUrl = 'localhost:5000/web';
+const dataProvider = jsonapiClient(apiUrl);
+
+
+export const createBadge = (token, id, name, picture, unlocked_picture, description, cost) => {
+    return `
+mutation {
+    createBadge(
+        token:"`+ token + `",
+        id:"`+ id + `" ,
+        name:"`+ name + `" ,
+        picture:"`+ picture + `",
+        unlocked_picture:"`+ unlocked_picture + `",
+        description:"`+ description + `",
+        cost:"`+ cost + `")
+    {
+ badge
+ {
+    id
+   name
+ }
+    }
+    }
+`
+}
+
+export const updateBadge = (token, id, name, picture, unlocked_picture, description, cost) => {
+    console.log(token, id, name, picture, unlocked_picture, description, cost )
+    return ` mutation {
+    updateBadge(
+        token:"`+ token + `",
+        id:"`+ id + `" ,
+        name:"`+ name + `" ,
+        picture:"`+ picture + `",
+        unlocked_picture:"`+ unlocked_picture + `",
+        description:"`+ description + `",
+        cost:"`+ cost + `")
+    {
+ badge
+ {
+    id
+   name
+    }
+}
+}
+`
+}
