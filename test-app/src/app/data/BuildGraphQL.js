052f9852a59c30289368ad898c6522975b22d06f
Anna Kugler <ar-annamaria.kugler@rakuten.com>
2021-03-10 14:42:09 +0100

    added gqlDataProvider

---
+++ test-app/src/app/data/BuildGraphQL.js
@@ -0,0 +1,38 @@
+import buildGraphQLProvider from 'ra-data-graphql-simple';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { RestLink } from 'apollo-link-rest';
+import simpleRestProvider from 'ra-data-simple-rest'
+import { AUTH_TOKEN } from './constants';
+
+const API_ENDPOINT = 'http://127.0.0.1:5000/web';
+const httpLink = new HttpLink({ uri: API_ENDPOINT });
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN);
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      }
+    }
+  });
+
+export const gqlClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
+
+export const gqlDataProvider = buildGraphQLProvider({
+    client: gqlClient
+  });
+
+// setup for using multiple providers 
+/* export const restClient = new ApolloClient({
+    link: new RestLink({ uri: LOCAL_REST_ENDPOINT }),
+    cache: new InMemoryCache()
+  });
+ */
+
+/*   const LOCAL_REST_ENDPOINT = 'http://127.0.0.1:5000/file';
+  export const RestProvider = simpleRestProvider(LOCAL_REST_ENDPOINT); */
