84cbe648636d075035ad1fb2e66e7519c14b91a1
mosouka <cosima.balzer@stud.tu-darmstadt.de>
2021-03-13 18:26:40 +0000

    index.js for deployment

--- test-app/src/index.js
+++ test-app/src/index.js
@@ -5,18 +5,27 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter as Router } from "react-router-dom";
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Path from 'path';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//App.use(Express.static(Path.join(__dirname, 'build'))); //here is important thing - no static directory, because all static :)
+
+/*App.get("/*", function(req, res) {
+  res.sendFile(Path.join(__dirname, 'build', "index.html"));
+});*/
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+//ReactDOM.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>,
+//  document.getElementById('root')
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
